099ff4ea219cbe5eb0adf9dbf6212e2cfd2df9c8
Habideen Ibrahim <habideenibrahim@gmail.com>
2023-03-12 17:59:34 +0100

    Initializing program structure

---
+++ models/base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Base Model Module
+Responsible for referencing other models
+"""
+
+class BaseModel:
+    """Base Model Class"""
+
+    def __init__(self, *args, **kwargs):
+        """Base model __init__ model"""
+        return True
---
+++ tests/test_base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Unittest for base model module.
+"""
+
+class TestBaseModel(unittest.TestCase):
+    """this tests the base model class"""
+    def __init__(self):
+        """Initialize with __init__"""
+        return True
+
+
+if __name__ == "__main__":
+    unittest.main()
