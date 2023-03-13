c316b733bc5043ab8d21dcaf5982b0d35f56e215
Abel-G <abeljeb@gmail.com>
2023-03-13 13:22:22 -0700

    print

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ this is a module containing the amenity class """
 
-from  models.base.model import BaseModel
+from models.base.model import BaseModel
 
 
 class Amenity(BaseModel):
