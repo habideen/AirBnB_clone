8d5189468c440e28f1cc80dd3c6897f7a44676fc
Abel-G <abeljeb@gmail.com>
2023-03-14 08:11:30 -0700

    completing the basemodel module

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,49 @@
 #!/usr/bin/python3
-""" this module contains the base model class """
+""" module containing the parent class """
 
 
-import models
 import uuid
-
+import models
 from datetime import datetime
 
 
 class BaseModel():
-    """ this is the baseModel class """
+    """ baseModel class """
 
     def __init__(self, *args, **kwargs):
-        """ initialization """
+        """ initializing  the class """
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    dt = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, dt)
+                    continue
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """ return string rep. of the class"""
+
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """ updating the public instance attr. update_at """
+
+        self.update_at = datetime.now()
+        modles.storage.save()
+
+    def to_dict(self):
+        """ return dictionery rep of  the keys and values of the instances """
+
+        dict = self.__dict__.copy()
+        dict["__class__"] = type(self).__name__
+        dict["created_at"] = dict["created_at"].isoformat()
+        dict["updated_at"] = dict["updated_at"].isoformat()
+        return dict
